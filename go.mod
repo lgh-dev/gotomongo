471b446ad42a37730e2f1229664cc8cbc42508c6
ss898 <18520640132@163.com>
2019-10-18 00:26:31 +0800

    测试go并发插入mongo

---
+++ go.mod
@@ -0,0 +1,8 @@
+module mongotest
+
+go 1.13
+
+require (
+	labix.org/v2/mgo v0.0.0-20140701140051-000000000287
+	launchpad.net/gocheck v0.0.0-20140225173054-000000000087 // indirect
+)
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
+	"strconv"
+	"time"
+)
+
+type Person struct {
+	Name  string
+	Phone string
+}
+
+func main() {
+	session, err := mgo.Dial("mongodb://localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("test").C("people")
+	n := 50
+	m := 200000 / n
+	insert(n, m, c)
+	query(n, m, err, c)
+}
+
+func insert(n int, m int, c *mgo.Collection) {
+	start := time.Now()
+	ch := make(chan int)
+	for i := 0; i < n; i++ {
+		go func() {
+			for j := 0; j < m; j++ {
+				c.Insert(&Person{"lgh" + strconv.Itoa(j), "+555381169639"})
+			}
+			ch <- 1
+		}()
+	}
+	//for range ch {
+	//	fmt.Println(n)
+	//	if n==1{
+	//		close(ch)
+	//	}
+	//	n--
+	//}
+	cost := time.Since(start)
+	fmt.Println("insert cost=[%s]", cost)
+}
+
+func query(n int, m int, err error, c *mgo.Collection) {
+	result := Person{}
+	start := time.Now()
+	ch := make(chan int)
+	for i := 0; i < n; i++ {
+		go func() {
+			for j := 0; j < m; j++ {
+				err = c.Find(bson.M{"name": "lgh" + strconv.Itoa(n-1)}).One(&result)
+			}
+			ch <- 1
+		}()
+	}
+	for range ch {
+		fmt.Println(n)
+		if n == 1 {
+			close(ch)
+		}
+		n--
+	}
+	cost := time.Since(start)
+	fmt.Println("query cost=[%s]", cost)
+	fmt.Println("Phone:", result.Phone)
+
+}
